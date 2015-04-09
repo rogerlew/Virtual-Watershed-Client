ac905bd4023127e480d78daef8efde31d8e69f2c
Shubham Gogna <s-gogna@user.noreply.github.com>
2015-04-09 13:25:04 -0700

    Started GeoRef materials

---
+++ NetworkTest/NetworkTest/Unity/GeoRefManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GeoRefManager
+{
+    // Fields
+    VWClient client;
+    Dictionary<String, GeoReference> stored = new Dictionary<string, GeoReference>();
+
+    // Constructors
+    public GeoRefManager(VWClient refToClient)
+    {
+        client = refToClient;
+    }
+
+    // Methods
+    public GeoReference request(string key)
+    {
+        return stored[key];
+    }
+
+    public void buildObject(string recordname)
+    {
+        // TODO
+    }
+
+    public void download(string recordname, string service = "vwc")
+    {
+        // TODO
+    }
+
+    public void getAvailable(string service = "vwc", int offset, string otherparams)
+    {
+        // TODO
+    }
+}
---
+++ NetworkTest/NetworkTest/Unity/GeoReference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GeoReference
+{
+    // Fields
+    String type;
+    List<DataRecord> recs;
+    GameObject obj;
+    Texture2D texture;
+
+    // Constructor
+    public GeoReference(String refType)
+    {
+        type = refType;
+    }
+
+    // Methods
+    public void updateRecords() 
+    { 
+        // TODO
+    }
+
+    public void build()
+    {
+        // TODO
+    }
+
+    // TODO getters? setters?
+}
