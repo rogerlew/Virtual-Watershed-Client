e5197c5a9f4b915df9ad41c9e4a2f5ea1ec28ebe
Thomas Rushton <trushton134@gmail.com>
2015-05-22 15:19:19 -0700

    Adding Unity files

---
+++ WatershedUnityBuild/Assets/Scripts/VariableRefrence.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableRefrence : MonoBehaviour {
+
+    // The path to the file with the variables names and description
+#if UNITY_EDITOR
+    const string MAPFILENAME = "VariableRefrenceFile.txt";
+    public static string DirectoryLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)+"/../../VariableMetaData";
+#else
+    const string MAPFILENAME = "VariableRefrenceFile.txt";
+    public static string DirectoryLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+#endif
+
+
+
+	// Use this for initialization
+	void Start () {
+	    
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
